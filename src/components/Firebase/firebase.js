be0a9246ebf6240263e03ea25daafab0945e7212
Juha Kovero <juha.kovero@starcut.com>
2018-12-04 11:41:44 +0200

    config

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 require('dotenv').config()
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyCcNXC25DjJcI9l7iKvIK0183VAWNxu9Ag",
+  authDomain: "kovepages.firebaseapp.com",
+  databaseURL: "https://kovepages.firebaseio.com",
+  projectId: "kovepages",
+  storageBucket: "kovepages.appspot.com",
+  messagingSenderId: "559337801228"
 };
 
 class Firebase {
